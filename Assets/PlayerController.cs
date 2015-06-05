01bcbc3a175716ea64543d000bab0ba095466567
KOTAAKAMATSU <redjuly2015@gmail.com>
2015-06-05 19:28:25 +0900

    できた！

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	Animator anim;
+
+	UnityChan.UnityChanControlScriptWithRgidBody unitychan;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		unitychan = GetComponent< UnityChan.UnityChanControlScriptWithRgidBody >();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGoal() {
+		unitychan.enabled = false;
+		anim.SetBool( "Win", true );
+	}
+}
